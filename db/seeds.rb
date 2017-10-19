914f88cd01732bc7a312b18d7ba84d3ea3db2529
Nam Kim <nryulkim@gmail.com>
2017-10-19 09:04:01 -0400

    Create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+collections = {
+  'Benewig Collection': {
+    'Benewig Full Wig': [
+      'Alicia', 'Halle_1', 'Keyshia', 'Mary', 'Nicki'
+    ]
+    'Benewig Lace Wig': [
+      'Ariana', 'Beyonce', 'Ciara2', 'Gina', 'Janet', 'Jetty', 'Kess',
+      'Mariah', 'Mya', 'Nana', 'Rihanna', 'Tama', 'Tyra', 'Whiteney'
+    ]
+  }
+  'Indu Gold': {
+    'Indu Gold Full Wig': ['Lena']
+    'Indu Gold Lace Wig': [
+      'Amery', 'Chanel', 'Cleo', 'Coco', 'Emma', 'Rhona', 'Ricca',
+      'Roma', 'Rose', 'Ruby', 'Sista', 'Sofia', 'Tess', 'Tiara', 'Vera',
+      'Vicky'
+    ]
+  }
+}
+
+Collection.destroy_all
+for name in collections.keys do
+  collection = Collection.create({ name: name })
+  wigs = collections[name]
+  for wig in wigs.keys do
+    wigobj = Wig.create({ name: wig, collection_id: collection.id })
+    for product in wigs[wig] do
+      Product.create({ name: product, wig_id: wigobj.id })
+    end
+  end
+end
