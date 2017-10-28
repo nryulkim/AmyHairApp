b0ce64051fe90cdcf6cd10ff04c11f1f7e3f3f2d
Nam Kim <nryulkim@gmail.com>
2017-10-28 07:59:18 -0400

    Remove Rack::Cors

--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,12 @@ Bundler.require(*Rails.groups)
 
 module AmyHair
   class Application < Rails::Application
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins 'localhost:8081'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
-      end
-    end
+    # config.middleware.insert_before 0, Rack::Cors do
+    #   allow do
+    #     origins 'localhost:8081'
+    #     resource '*', :headers => :any, :methods => [:get, :post, :options]
+    #   end
+    # end
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
